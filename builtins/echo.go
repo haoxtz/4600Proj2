4e5c64b383f8dae9c11475f3b971e5a74125f43f
haoxtz <123910289+haoxtz@users.noreply.github.com>
2023-05-11 02:16:30 -0500

    Comments added

--- builtins/echo.go
+++ builtins/echo.go
@@ -5,17 +5,18 @@ import (
 	//"os"
 )
 
+/*
+echo in CSH(the C shell) as described in https://docstore.mik.ua/orelly/linux/lnut/ch08_09.htm
+echo [-n] string
+Write string to standard output; if -n is specified, the output is not terminated by a newline.
+... the C shell's echo doesn't support escape characters.
+*/
 func EchoCSH(args ...string) error {
-	/*
-		echo in CSH(the C shell) as described in https://docstore.mik.ua/orelly/linux/lnut/ch08_09.htm
-		echo [-n] string
-		Write string to standard output; if -n is specified, the output is not terminated by a newline.
-		... the C shell's echo doesn't support escape characters.
-	*/
-	if args[0] == "-n" {
+	// checking if -n flag is raised
+	if args[0] == "-n" { //if so, removing before proceeding with echoing WITHOUT newline
 		args = args[1:]
 		fmt.Println(args)
-	} else {
+	} else { //else can echo as is WITH newline
 		fmt.Println(args)
 		fmt.Println()
 	}
